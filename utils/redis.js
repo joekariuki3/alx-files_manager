eeb35f5f329e4bb23de34f5cc066ad0400212a97
joekariuki3 <joekariuki3@gmail.com>
2024-03-10 15:00:55 +0300

    Redis utils
    fixed lint error

--- utils/redis.js
+++ utils/redis.js
@@ -4,47 +4,58 @@ import redis from 'redis';
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-	this.client.on('error', (error) => { console.log(error); });
+    this.client.on('connect', () => {});
+    this.client.on('error', (error) => {
+      console.log(error);
+    });
   }
 
+  // check if client is connected to redis
   isAlive() {
-	  return this.client.connected;
+    return this.client.connected;
   }
 
+  // retrieves value of key passed from redis
   async get(key) {
-	  return new Promise((resolve, reject) => {
-		  this.client.get(key, (error, reply) => {
-			  if (error) {
-				  reject(error);
-				  return;
-			  }
-			  resolve(reply);
-		  });
-	  });
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, reply) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(reply);
+      });
+    });
   }
 
+  // sets a expiry duration of a value set for key
   async set(key, value, duration) {
-		return new Promise((resolve, reject) => {
-			this.client.set(key, value, 'EX',  duration, (error, reply) => {
-				if (error) {
-					reject(error);
-					return;
-				}
-				resolve(reply);
-			});
-		});
-	}
+    return new Promise((resolve, reject) => {
+      this.client.set(key, value, 'EX', duration, (error, reply) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(reply);
+      });
+    });
+  }
+
+  // Removes value of the key passed to it
   async del(key) {
-		return new Promise((resolve, reject) => {
-			this.client.del(key, (error, reply) => {
-				if (error) {
-					reject(error);
-					return;
-				}
-				resolve(reply);
-			});
-		});
-	}
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error, reply) => {
+        if (error) {
+          reject(error);
+          return;
+        }
+        resolve(reply);
+      });
+    });
+  }
 }
-const redisClient = new RedisClient()
-module.exports = redisClient
+
+// instance of RedisClient
+const redisClient = new RedisClient();
+// export the created instance
+module.exports = redisClient;
