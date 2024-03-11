c31ba3950a5cbcf1bd87447bc2c85d1db6298676
joekariuki3 <joekariuki3@gmail.com>
2024-03-11 15:45:08 +0300

    Create a new user

---
+++ controllers/UsersController.js
@@ -0,0 +1,35 @@
+import sha1 from 'sha1';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(request, response) {
+    const { email, password } = request.body;
+    if (!email) {
+      response.status(400).json({ error: 'Missing email' });
+    }
+    if (!password) {
+      response.status(400).json({ error: 'Missing password' });
+    }
+
+    const hashPwd = sha1(password);
+
+    try {
+      const collection = dbClient.db.collection('users');
+      const user1 = await collection.findOne({ email });
+
+      if (user1) {
+        response.status(400).json({ error: 'Already exist' });
+      } else {
+        collection.insertOne({ email, password: hashPwd });
+        const newUser = await collection.findOne(
+          { email }, { projection: { email: 1 } },
+        );
+        response.status(201).json({ id: newUser._id, email: newUser.email });
+      }
+    } catch (error) {
+      console.log(error);
+      response.status(500).json({ error: 'Server error' });
+    }
+  }
+}
+module.exports = UsersController;
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,16 @@ const express = require('express');
 
 const router = express.Router();
 const AppController = require('../controllers/AppController');
+const UsersController = require('../controllers/UsersController');
 
 router.get('/status', (req, res) => {
   AppController.getStatus(req, res);
 });
 router.get('/stats', (req, res) => {
   AppController.getStats(req, res);
+});
+router.post('/users', (req, res) => {
+	UsersController.postNew(req, res)
 });
 
 module.exports = router;
