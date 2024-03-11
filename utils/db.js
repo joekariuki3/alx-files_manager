a830010b3ff88bc574201c2cd508b908967866d5
joekariuki3 <joekariuki3@gmail.com>
2024-03-11 12:43:50 +0300

    MongoDB utils correction

--- utils/db.js
+++ utils/db.js
@@ -1,26 +1,36 @@
 const { MongoClient } = require('mongodb');
 
+// get data from environment variable if they are set
+const host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${host}:${port}/`;
+
 class DBClient {
   constructor() {
-    this.host = process.env.DB_HOST || 'localhost';
-    this.port = process.env.DB_PORT || 27017;
-    this.database = process.env.DB_DATABASE || 'files_manager';
-    this.client = new MongoClient(`mongodb://${this.host}:${this.port}/${this.database}`, { useUnifiedTopology: true });
+    this.db = null;
+    // make the connection to the db when instance of client is created
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      // handle error during connection
+      if (error) {
+        console.log(error);
+      }
+      // connect to our db
+      this.db = client.db(database);
+      // create users and files collections
+      this.db.createCollection('users');
+      this.db.createCollection('files');
+    });
   }
 
-  async isAlive() {
-    try {
-      await this.client.connect();
-      return true;
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error);
-      return false;
-    }
+  isAlive() {
+    // use !! to trnsform return value to boolean(true/false)
+    return !!this.db;
   }
 
   async nbUsers() {
     try {
-      const collection = this.client.db(this.database).collection('users');
+      const collection = this.db.collection('users');
       const count = await collection.countDocuments();
       return count;
     } catch (error) {
@@ -31,7 +41,7 @@ class DBClient {
 
   async nbFiles() {
     try {
-      const collection = this.client.db(this.database).collection('files');
+      const collection = this.db.collection('files');
       const count = await collection.countDocuments();
       return count;
     } catch (error) {
