dfe7a683dffed8f135ccc684ccd77b3bdc5aa0e6
bernadettestella <bernadettestella27@gmail.com>
2024-03-13 12:18:36 +0300

    File Manager

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -19,7 +19,6 @@ class FilesController {
     const userId = await findUserIdByToken(request);
     if (!userId) return response.status(401).json({ error: 'Unauthorized' });
 
-    let fileInserted;
 
     // Validate the request data
     const { name } = request.body;
@@ -39,6 +38,8 @@ class FilesController {
       if (file.type !== 'folder') return response.status(400).json({ error: 'Parent is not a folder' });
     }
 
+    let fileInserted;
+    
     // if no data, and not a folder, error
     if (!data && type !== 'folder') return response.status(400).json({ error: 'Missing Data' });
 
@@ -106,6 +107,20 @@ class FilesController {
     const idFile = request.params.id || '';
     const fileDocument = await dbClient.db.collection('files').findOne({ _id: ObjectID(idFile), userId: user._id });
     if (!fileDocument) return response.status(404).send({ error: 'Not found' });
+
+    const size = parseInt(request.query.size) || 0;
+    const filePath = path.join(process.env.FOLDER_PATH, fileDocument.localPath);
+    const thumbnailPath = size > 0 ? `${filePath}_${size}` : filePath;
+
+    if (!fs.existsSync(thumbnailPath)) {
+       return response.status(404).send({ error: 'Not found' });
+    }
+
+    const fileData = fs.readFileSync(thumbnailPath);
+    const mimeType = mime.lookup(filePath);
+
+    res.setHeader('Content-Type', mimeType);
+    res.send(fileData);
 
     return response.send({
       id: fileDocument._id,
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const fileQueue = require('./worker');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/', routes);
+
+fileQueue.init();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
